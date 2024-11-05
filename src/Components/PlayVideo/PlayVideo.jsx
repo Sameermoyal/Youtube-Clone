208197c90cc49ebd5088b88d213dbd1b0ea61a89
Sameer  Moyal <sameerab827@gmail.com>
2024-11-05 11:46:29 +0530

    make comment fetch from api

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -9,11 +9,15 @@ import jack from "../../assets/jack.png"
 import user_profile from "../../assets/user_profile.jpg"
 import { API_KEY, value_converter } from '../../Data'
 import moment from 'moment'
+import { useParams } from 'react-router-dom'
 
-const PlayVideo = ({videoId}) => {
+const PlayVideo = () => {
   
+  const{videoId}=useParams()
+
   const [apiData,setApiData]=useState(null)
   const[channelData,setChannelData]=useState(null)
+  const[commentData,setCommnetdata]=useState([])
 
   const fetchVideoData=async()=>{
     //Fething Videos Data
@@ -29,12 +33,19 @@ const PlayVideo = ({videoId}) => {
 
     const channelData_url=` https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${apiData.snippet.channelId}&key=${API_KEY}`
     await fetch(channelData_url).then(res=>res.json()).then(data=>setChannelData(data.items[0]))
+    
+     //fetchimg comment data
+
+    const comment_url =`https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${videoId}&key=${API_KEY}
+`
+    await fetch(comment_url).then(res=>res.json()).then(data=>setCommnetdata(data.items))
 
   }
 
+ 
   useEffect(()=>{
     fetchVideoData();
-  },[])
+  },[videoId])
   useEffect(()=>{
     fetchOtherData();
   },[apiData])
@@ -66,58 +77,24 @@ const PlayVideo = ({videoId}) => {
           <p>{apiData?apiData.snippet.description.slice(0,250):"Description Here"}</p>
           <hr />
           <h4>{apiData?value_converter(apiData.statistics.commentCount):105} Comments</h4>
-          <div className="comment">
-            <img src={user_profile} alt="" />
-            <div>
-              <h3>Jack Nicholsan <span>1 day ago</span></h3>
-              <p>please provide mode computer providing information </p>
-              <div className="comment-action">
-                <img src={like} alt="" />
-                <span>244</span>
-                <img src={dislike} alt="" />
-              </div>
-           
-          </div>
-          </div>
-          <div className="comment">
-            <img src={user_profile} alt="" />
-            <div>
-              <h3>Jack Nicholsan <span>1 day ago</span></h3>
-              <p>please provide mode computer providing information </p>
-              <div className="comment-action">
-                <img src={like} alt="" />
-                <span>244</span>
-                <img src={dislike} alt="" />
-              </div>
-           
-          </div>
-          </div>
-          <div className="comment">
-            <img src={user_profile} alt="" />
-            <div>
-              <h3>Jack Nicholsan <span>1 day ago</span></h3>
-              <p>please provide mode computer providing information </p>
-              <div className="comment-action">
-                <img src={like} alt="" />
-                <span>244</span>
-                <img src={dislike} alt="" />
-              </div>
-           
-          </div>
-          </div>
-          <div className="comment">
-            <img src={user_profile} alt="" />
-            <div>
-              <h3>Jack Nicholsan <span>1 day ago</span></h3>
-              <p>please provide mode computer providing information </p>
-              <div className="comment-action">
-                <img src={like} alt="" />
-                <span>244</span>
-                <img src={dislike} alt="" />
-              </div>
-           
-          </div>
-          </div>
+          {commentData.map((item,index)=>{
+            return(
+              <div key={index} className="comment">
+              <img src={item.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="" />
+              <div>
+                <h3>{item.snippet.topLevelComment.snippet.authorDisplayName} <span>1 day ago</span></h3>
+                <p>{item.snippet.topLevelComment.snippet.textDisplay} </p>
+                <div className="comment-action">
+                  <img src={like} alt="" />
+                  <span>{value_converter(item.snippet.topLevelComment.snippet.likeCount)}</span>
+                  <img src={dislike} alt="" />
+                </div>
+             
+            </div>
+            </div>
+            )
+          })}
+         
         </div>
     </div>
   )
